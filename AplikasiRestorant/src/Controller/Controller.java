8852776d90ca695b65e24fb98b50f2bf7f760182
Asus <Asus@LAPTOP-2KDOGGR9>
2020-11-22 23:04:25 +0700

    update controller

--- AplikasiRestorant/src/Controller/Controller.java
+++ AplikasiRestorant/src/Controller/Controller.java
@@ -5,13 +5,21 @@
  */
 package Controller;
 
+import Model.Meja;
+import Model.Membership;
 import Model.Order;
+import Model.Person;
+import Model.Reservasi;
 import Model.stockMakanan;
+import View.screenMembership;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JButton;
 
 /**
  *
@@ -19,41 +27,37 @@ import java.util.ArrayList;
  */
 public class Controller {
     static DatabaseHandler conn = new DatabaseHandler();
-
     
-    
-    //SELECT ALL from table stockMakanan
-    public ArrayList<stockMakanan> getAllstock(){
-        ArrayList<stockMakanan> stockmakanan = new ArrayList<>();
+    public static boolean Reservasi(Reservasi nreservasi){
         conn.connect();
-        String query = "SELECT * FROM stockmakanan";
+        String query = "INSERT INTO reservasi VALUES (?,?,?,?,?,?)";
         try{
-            Statement stmt = conn.con.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            while(rs.next()){
-                stockMakanan Stock = new stockMakanan();
-                Stock.setID_Makanan(rs.getString("ID_Makanan"));
-                Stock.setNama(rs.getString("Nama"));
-                Stock.setJumlah(rs.getInt("Jumlah"));
-                Stock.setHarga(rs.getDouble("Harga"));
-                stockmakanan.add(Stock);
-            }
-        }catch(SQLException e){
-            e.printStackTrace();
+            PreparedStatement stmt = conn.con.prepareStatement(query);
+            stmt.setString(1, nreservasi.getID());
+            stmt.setString(2, nreservasi.getNama());
+            stmt.setString(3, nreservasi.getTanggal_Reservasi());
+            stmt.setInt(4, nreservasi.getJumlahOrang());
+            stmt.setString(5, nreservasi.getNo_Telp());
+            stmt.setString(6, nreservasi.getID_Meja());
+            stmt.executeUpdate();
+            return(true);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return(false);
         }
-        return(stockmakanan);
     }
-    public static boolean Membership(String ID_Member, String Nama, String Tl, String Email, String No_Telp, String Tanggal_ExpDate){
+    
+    public static boolean Membership(Membership member){
         conn.connect();
-        String query = "INSERT INTO membership VALUES (?,?,?,?,?,?)";
+        String query = "INSERT INTO members VALUES (?,?,?,?,?,?)";
         try {
             PreparedStatement stmt = conn.con.prepareStatement(query);
-            stmt.setString(1,ID_Member );
-            stmt.setString(2, Nama);
-            stmt.setString(3, Tl);
-            stmt.setString(4, Email);
-            stmt.setString(5, No_Telp);
-            stmt.setString(6, Tanggal_ExpDate);
+            stmt.setString(1, member.getID_Member());
+            stmt.setString(2, member.getNama());
+            stmt.setString(3, member.getNo_Telp());
+            stmt.setString(4, member.getTl());
+            stmt.setString(5, member.getEmail());
+            stmt.setString(6, member.getTanggal_ExpDate());
             stmt.executeUpdate();
             return(true);
         } catch (SQLException e) {
@@ -61,17 +65,15 @@ public class Controller {
         }
         return(false);
     }
-    public static boolean Reservasi(String ID, String Nama, int JumlahOrang, String No_Telp, String Jenis_Meja,String Tanggal_Reservasi){
+    
+    public static boolean Person(Person persons){
         conn.connect();
-        String query = "INSERT INTO Reservasi VALUES (?,?,?,?,?,?)";
+        String query = "INSERT INTO Person VALUES (?,?,?)";
         try {
             PreparedStatement stmt = conn.con.prepareStatement(query);
-            stmt.setString(1, ID);
-            stmt.setString(2, Nama);
-            stmt.setInt(3, JumlahOrang);
-            stmt.setString(4, No_Telp);
-            stmt.setString(5, Jenis_Meja);
-            stmt.setString(6, Tanggal_Reservasi);
+            stmt.setString(1, persons.getID_Person());
+            stmt.setString(2, persons.getNama());
+            
             stmt.executeUpdate();
             return(true);
         } catch (SQLException e) {
@@ -79,6 +81,23 @@ public class Controller {
         }
         return(false);
     }
+    
+    public static boolean insertOrder(Order order){
+        conn.connect();
+        String query = "INSERT INTO orders VALUES (?,?)";
+        try {
+            PreparedStatement stmt = conn.con.prepareStatement(query);
+            stmt.setInt(1, order.getJumlah_Makanan());
+            stmt.setInt(1, order.getJumlah_Makanan());
+            stmt.executeUpdate();
+            return(true);
+        } catch (SQLException ex) {
+            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+            return(false);
+        } 
+    }
+    
+    
     public static boolean Meja( String ID_Meja,String Jenis_Meja,int No_Meja){
         conn.connect();
         String query = "INSERT INTO Meja VALUES (?,?,?,?,?,?)";
@@ -87,7 +106,6 @@ public class Controller {
             stmt.setString(1, ID_Meja);
             stmt.setString(2, Jenis_Meja);
             stmt.setInt(3, No_Meja);
-            
             stmt.executeUpdate();
             return(true);
         } catch (SQLException e) {
@@ -152,20 +170,45 @@ public class Controller {
         }
         return(false);
     }
-    public static boolean Person( String ID_Person,String nama,String Status){
+    
+    //SELECT ALL from table meja
+    public static ArrayList<Meja> getAllMeja(){
+       ArrayList<Meja> meja = new ArrayList<>();
+       conn.connect();
+       String query = "SELECT * FROM meja";
+       try{
+           Statement stmt = conn.con.createStatement();
+           ResultSet rs = stmt.executeQuery(query);
+           while(rs.next()){
+               Meja new_meja = new Meja();
+               new_meja.setID_Meja(rs.getString("ID_meja"));
+           }
+       } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+       return(meja);
+    }
+    
+    
+    //SELECT ALL from table stockMakanan
+    public ArrayList<stockMakanan> getAllstock(){
+        ArrayList<stockMakanan> stockmakanan = new ArrayList<>();
         conn.connect();
-        String query = "INSERT INTO Person VALUES (?,?,?)";
-        try {
-            PreparedStatement stmt = conn.con.prepareStatement(query);
-            stmt.setString(1, ID_Person);
-            stmt.setString(2, nama);
-            stmt.setString(3, Status);
-            
-            stmt.executeUpdate();
-            return(true);
-        } catch (SQLException e) {
+        String query = "SELECT * FROM stockmakanan";
+        try{
+            Statement stmt = conn.con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while(rs.next()){
+                stockMakanan Stock = new stockMakanan();
+                Stock.setID_Makanan(rs.getString("ID_Makanan"));
+                Stock.setNama(rs.getString("Nama"));
+                Stock.setJumlah(rs.getInt("Jumlah"));
+                Stock.setHarga(rs.getInt("Harga"));
+                stockmakanan.add(Stock);
+            }
+        }catch(SQLException e){
             e.printStackTrace();
         }
-        return(false);
+        return(stockmakanan);
     }
 }
