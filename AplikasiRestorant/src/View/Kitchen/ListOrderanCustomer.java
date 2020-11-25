c379ff98c21180b98638d49364e2b6d9b5eba639
Femi Elice <femisimbolon00@gmail.com>
2020-11-25 13:17:04 +0700

    modified listorderancustomer

---
+++ AplikasiRestorant/src/View/Kitchen/ListOrderanCustomer.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View.Kitchen;
+
+import View.Screen;
+import View.ScreenCustomers;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+/**
+ *
+ * @author willi
+ */
+public class ListOrderanCustomer {
+    public ListOrderanCustomer() {
+        JFrame frame = new JFrame("Orderan Customer");
+        frame.setSize(500, 700);
+        frame.setLocationRelativeTo(null);   
+}
+}
